108ddbae0b9ea160279d914f000a500feb5657a5
Alex Bard <alex@bardworx.com>
2021-04-22 14:22:19 -0400

    - Moved player and sensor to their own files. - Updated logic to throw an error when sensor is not initialized. - Updated .gitignore
    Signed-off-by: Alex Bard <alex@bardworx.com>

--- main.py
+++ main.py
@@ -2,90 +2,50 @@
 
 # LIBRARIES
 import sys
-import RPi.GPIO as GPIO
 import time
-from omxplayer.player import OMXPlayer
+from src import sensor
+from src import player
 from pathlib import Path
 
-# Set constant values.
-# Any variables in all caps should not be changed whent he program is running.
+# VARIABLES
+VIDEO_PATH = Path("/home/pi/grandpa.mp4")  # Path to video
+DISTANCE_SENSITIVITY = 10  # Distance at which to trigger the video (in cm)
 
-# Set pins that are connected to RPI 
-PIN_TRIGGER = 7
-PIN_ECHO = 11
 
-# Default video path
-VIDEO_PATH = Path("/home/pi/grandpa.mp4")
+def main():
+    try:
+        omx_player = player.initialize(VIDEO_PATH)
 
-# Distance at which to trigger the video (in cm)
-DISTANCE_SENSITIVITY = 10
+        while True:
+            omx_player.pause()
+            try:
+                calculated_distance = sensor.calculate_distance()
+                if calculated_distance <= DISTANCE_SENSITIVITY:
+                    omx_player.play()
+                    time.sleep(omx_player.duration())
+                else:
+                    pass
 
-# GPIO SETUP
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-GPIO.setup(PIN_ECHO, GPIO.IN)
+                omx_player.set_position(0.0)
+            except ValueError:
+                print("--- ERROR: Sensor was not initialized. ---")
+                end_program()
 
-# DEBUG values
-play_count = 0
+    except KeyboardInterrupt:
+        end_program()
 
-# Calculate distance via Sonar sensor
-def distance():
-    # Setting the trigger to low allows the sensor to settle
-    # per tutorial, this should provide more consistent results.
-    # GPIO.output(PIN_TRIGGER, GPIO.LOW)
-    # time.sleep(2)
-    #
-    # I commented this section out as I'm not sure how useful this is in Live Portrait application
-    
-    print("Calculating distance...")
-    GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-    
-    # Timer to allow the sensor to receive a reading
-    time.sleep(0.00001)
-    
-    # Turn off sensor to process reading
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
-    
-    # Process returned data
-    while GPIO.input(PIN_ECHO) == 0:
-        pulse_start_time = time.time()
-    while GPIO.input(PIN_ECHO) == 1:
-        pulse_end_time = time.time()
 
-    # OUTPUT
-    pulse_duration = pulse_end_time - pulse_start_time
-    # durantion * sonic speed (34300 cm/s)
-    # divide by 2 since we're doing a round trip (there/back)
-    distance = round(pulse_duration * 34300 / 2, 2)
-    print("Distance = %.1f cm" % distance)
-    
-    return distance
+def end_program():
+    """Close application with keyboard input.
 
-try:
-    player = OMXPlayer(VIDEO_PATH, args=['--no-osd', '--loop'])
-    # sleep is here for the player to have enough time to load the video into the buffer.
-    # different videos may require different buffer time.
-    # NOTE: I noticed that a longer sleep timer causes the video to start playing before pausingm adjust as necessary.
-    time.sleep(0.55)
-    print("Ready to trigger")
-    
-    # Program loop
-    while True:
-        player.pause()
-        calculated_distance = distance()
-        
-        if( calculated_distance <= DISTANCE_SENSITIVITY):
-            print("Trigger count {}".format(play_count))
-            player.play()
-            time.sleep(player.duration())
-            play_count += 1
-        else:
-            pass
-        
-        player.set_position(0.0)
-        
-except KeyboardInterrupt:
-    player.quit()
-    GPIO.cleanup()
+    To exit, hit Control + X. You may have to hit it twice.
+    """
+
+    print("STATUS: Closing Program")
+
+    player.close()
+    sensor.cleanup()
     sys.exit()
-        
+
+
+main()
---
+++ src/sensor.py
@@ -0,0 +1,80 @@
+"""This file contains all the constants that are used in the main
+program for organization purposes.
+
+
+Args:
+    PIN_TRIGGER: corresponds to the actual pin on the raspberry pi.
+      This is how the program knows what to listen to.
+    PIN_ECHO: corresponds to the actual pin on the raspberry pi.
+      This is how the program knows where to send the signal.
+"""
+
+import time
+import RPi.GPIO as GPIO
+
+_INITIALIZED = False  # track if the sensor was initialized.
+
+# Set pins that are connected to RPI
+_PIN_TRIGGER = 7
+_PIN_ECHO = 11
+
+
+def initialize():
+    """Initializes the sensor for more consistent reading on startup.
+
+    Returns:
+        sensor status.
+    """
+
+    global _INITIALIZED
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(_PIN_TRIGGER, GPIO.OUT)
+    GPIO.setup(_PIN_ECHO, GPIO.IN)
+
+    if not _INITIALIZED:
+        GPIO.output(_PIN_TRIGGER, GPIO.LOW)
+        time.sleep(2)
+        _INITIALIZED = True
+
+    return _INITIALIZED
+
+
+def calculate_distance():
+    """Calculates the distance via Sonar sensor.
+
+    Raises:
+        ValueError - sensor was not initialized
+    Returns:
+         detected distance in centimeters
+    """
+    pulse_start_time = 0
+    pulse_end_time = 0
+
+    sensor_status = initialize()
+
+    if not sensor_status:
+        raise ValueError('ERROR - Sensor is not initialized!!!')
+
+    GPIO.output(_PIN_TRIGGER, GPIO.HIGH)
+    time.sleep(0.00001)  # Have to wait to receive signal.
+    GPIO.output(_PIN_TRIGGER, GPIO.LOW)
+
+    # Determine if current signal is start or end time.
+    while GPIO.input(_PIN_ECHO) == 0:
+        pulse_start_time = time.time()
+    while GPIO.input(_PIN_ECHO) == 1:
+        pulse_end_time = time.time()
+
+    # Formula: duration * sonic speed (34300 cm/s) divided by 2 .
+    # The division is because we have to consider its a round trip.
+    return round((pulse_end_time - pulse_start_time) * 34300 / 2, 2)
+
+
+def cleanup():
+    """Releases the GPIO pins.
+    If this is not performed, Python will throw a warning the next time
+    you run the program.
+    """
+
+    GPIO.cleanup()
